b38f9c24b44f1b8655a501e31e473445c04d5cda
koop4 <dilillo.roberto@gmail.com>
2020-05-24 23:42:10 +0200

    Updates

---
+++ precache-manifest.bacaa6bb12b5e823f0aa76430a0d1cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5494bbc7433d7bac8319deffc7d4918c",
+    "url": "/mimoma/index.html"
+  },
+  {
+    "revision": "b3831f37d84920acc229",
+    "url": "/mimoma/static/css/main.1413e578.chunk.css"
+  },
+  {
+    "revision": "c58b0aa845d4456b4c4b",
+    "url": "/mimoma/static/js/2.588f0e96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mimoma/static/js/2.588f0e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3831f37d84920acc229",
+    "url": "/mimoma/static/js/main.3ed35c1f.chunk.js"
+  },
+  {
+    "revision": "95167f187076ec1317e2",
+    "url": "/mimoma/static/js/runtime-main.fae0e54f.js"
+  }
+]);
